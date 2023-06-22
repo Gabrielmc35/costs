fa2def2109c0fecc353e9ba8c46988f2e3c311be
Gabrielmc35 <gabrielmc35@hotmail.com>
2023-06-22 17:41:00 -0300

    Navbar + Footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom"
+import {FaFacebook, FaInstagram, FaLink, FaLinkedin} from 'react-icons/fa'
+import styles from "./Footer.module.css"
+function Footer (){
+return(
+
+    <footer className={styles.footer}>
+        <ul className={styles.social_list}>
+            <li className={styles.iconApp}>
+            <Link to ="https://www.facebook.com/gabriel.magalhaes.180"> 
+            <FaFacebook/></Link>
+              
+            </li>
+            <li>
+             
+                <Link to ="https://www.instagram.com/gabrielmc35/"> 
+            <FaInstagram/></Link>
+            </li>
+            <li>
+              
+                <Link to ="https://www.linkedin.com/in/gabrielmc35/"> 
+            <FaLinkedin/></Link>
+            </li>
+
+        </ul>
+        <p className={styles.copyright}>
+            <span> Gabrielmc35 &copy; 2023</span> 
+            </p>
+    </footer>
+)
+}
+
+export default Footer
