ee407fd5288645b75cef82f0bd35312878c40580
Gabrielmc35 <gabrielmc35@hotmail.com>
2023-06-26 14:37:58 -0300

    Home ok

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,17 @@
+import styles from "../pages/Home.module.css"
+import savings from '../../img/savings.svg'
+import LinkButton from "../layout/LinkButton"
 function Home () {
 
     return (
-        <div>
-            <h1>Home</h1>
-          
-        </div>
+       
+        <section className = {styles.home_container}>
+            <h1>Bem vindo ao <span>Costs</span></h1>
+            <p>Gerencie seus projetos agora</p>
+            <LinkButton to ="/newproject" text ="Criar novo Projeto"/>
+            <img src={savings} alt="costs"/>
+        </section>
     )
     }
-    
+     
     export default Home
